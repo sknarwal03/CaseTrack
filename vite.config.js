4c4ceda761bd6e62f77cde9d356bdb30327d0609
Shivkumar Narwal <sknarwal03@gmail.com>
2026-09-13 18:47:46 +0530

    Configure Github Pages deployement

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "/CaseTrack",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        dashboard: resolve(__dirname, "pages/dashboard.html"),
+        cases: resolve(__dirname, "pages/cases.html"),
+        calendar: resolve(__dirname, "pages/calendar.html"),
+        poCases: resolve(__dirname, "pages/po-cases.html"),
+        report: resolve(__dirname, "pages/report.html"),
+        settings: resolve(__dirname, "pages/settings.html")
+      }
+    }
+  }
+});
